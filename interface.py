03a38daa312aba146cf7179377690f2fc6b06b2d
gmanatole <99678818+gmanatole@users.noreply.github.com>
2023-10-03 18:33:36 +0200

    Update interface.py for windows cds file
    Code was implemented in cds code but not in interface.py

--- interface.py
+++ interface.py
@@ -499,7 +499,12 @@ class Fenetre(QWidget):
 		if not os.path.isdir('data') :
 			os.mkdir('data')
 		self.loading_bar_era.setValue(1)
-		make_cds_file(self.key.text(), self.uid.text())
+		if os.name == 'nt' :
+			make_windows_cds_file(self.key.text(), self.uid.text())
+		elif os.name == 'posix':
+			make_cds_file(self.key.text(), self.uid.text())
+		else :
+			raise NotImplementedError('Acoustic wind toolbox not implemented for systems other than Windows or Linux')
 		self.loading_bar_era.setValue(2)
 		df = download_era_data(os.path.join(os.getcwd(), "data"), "era", self.key, ['10m_u_component_of_wind', '10m_v_component_of_wind'],
 			self.years.text().replace(" ", "").split(','), months, days, ['00:00','01:00','02:00','03:00','04:00','05:00','06:00', '07:00','08:00','09:00','10:00','11:00','12:00', '13:00','14:00','15:00','16:00','17:00','18:00','19:00','20:00','21:00','22:00','23:00'], 
